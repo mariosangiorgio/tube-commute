432eff3064e6cd19e5e0c0ac15f1d032646a1c15
Mario Sangiorgio <mariosangiorgio@gmail.com>
2016-12-25 18:49:21 +0000

    Added weekdays and code to extract a commute leg

--- commute.r
+++ commute.r
@@ -11,6 +11,7 @@ load_data <- function(file){
 	# As far as I can tell, this includes auto-topup and bus rides
 	tfl_data <- subset(tfl_data, End.Time != "")
 	tfl_data$Date <- as.Date(tfl_data$Date, format = "%d-%b-%Y")
+	tfl_data$Weekday <- weekdays(tfl_data$Date)
 	# Converts strings like 08:30 to the corresponding minutes from midnight
 	as.MinutesSinceMidnight <- function(hour){
 		tokens <- strsplit(hour, ':')[[1]]
@@ -23,5 +24,21 @@ load_data <- function(file){
 	tfl_data <- tfl_data %>% separate(Journey.Action, c("From", "To"), " to ")
 	tfl_data$From <- factor(tfl_data$From)
 	tfl_data$To <- factor(tfl_data$To)	
-	return(subset(tfl_data, select= c(Date, Start.Time, End.Time, Duration, From, To)))
+	return(subset(tfl_data, select= c(Date, Weekday, Start.Time, End.Time, Duration, From, To)))
+}
+
+# Extracts a commute leg from the data collected from the TFL website.
+# from and to are the two stations for the leg
+# start time is the hour from which rides are considered to be part of a commute.
+# if you want to consider rides that started from 8am to 9am you should specify
+# start.time = 8 and interval = 60
+extract_commute_leg <- function(tfl_data, from, to, start.time, interval = 60){
+	interval.start <- start.time * 60
+	interval.end <- start.time * 60 + interval
+	tfl_data <- subset(tfl_data,
+					(From == from) &					
+					(To == to) &
+					(Start.Time > interval.start) &
+					(Start.Time < interval.end) )
+	return(tfl_data)
 }
